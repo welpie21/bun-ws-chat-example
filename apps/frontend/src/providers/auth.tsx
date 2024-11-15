bb95c55a02730a067aa0f0c26c8b6fbffb356828
Beau den Heijer <beau@yaacomm.com>
2024-11-15 16:50:47 +0100

    authentication progression

--- apps/frontend/src/providers/auth.tsx
+++
@@ -1,6 +0,0 @@
-import { Accessor } from "solid-js";
-
-export interface AuthContextInterface {
-	token: Accessor<string | null>;
-	user: Accessor<string | null>;
-}
--- apps/frontend/src/stores/auth.ts
+++ apps/frontend/src/stores/auth.ts
@@ -1,7 +1,34 @@
 import type { RecordId } from "@surrealdb/surrealdb";
 import type { Optional } from "../types";
+import { createStore } from "solid-js/store";
 
-export interface AuthStore {
+export interface IAuthStore {
 	token: Optional<string>;
 	user: Optional<RecordId>;
 }
+
+const [store, setStore] = createStore<IAuthStore>({
+	token: undefined,
+	user: undefined,
+});
+
+export async function login(
+	_username: string,
+	_password: string
+) {
+
+}
+
+export async function register(
+	_username: string,
+	_password: string
+) {
+
+}
+
+export async function logout(): Promise<boolean> {
+	return false;
+}
+
+export const authStore = store;
+export const setAuthStore = setStore;
